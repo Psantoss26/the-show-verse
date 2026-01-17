33d5c42d45ea50c2557a75cb367e0c577194521c
Pablo Santos <pablosantoshernandez26@gmail.com>
2026-01-17 17:45:20 +0100

    solución deployment vercel

--- src/app/share/movie/[id]/page.jsx
+++ src/app/share/movie/[id]/page.jsx
@@ -2,7 +2,7 @@ import Link from 'next/link'
 import { notFound } from 'next/navigation'
 
 export const runtime = 'nodejs'
-export const revalidate = 60 * 60 * 24 // 24h
+export const revalidate = 86400
 
 const SITE_NAME = 'The Show Verse'
 
