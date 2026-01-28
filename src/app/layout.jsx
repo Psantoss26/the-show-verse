55ccb32d5152d74d3a0a853ea8b71c4cae988b7c
Pablo Santos <pablosantoshernandez26@gmail.com>
2026-01-28 19:04:11 +0100

    cambio logo

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -18,10 +18,10 @@ export const metadata = {
   description: 'Tu plataforma de películas y series',
   icons: {
     icon: [
-      { url: '/favicon.ico' }, // pestañas + bookmarks (clásico)
-      { url: '/TheShowVerse2.png', type: 'image/png', sizes: '32x32' }, // fallback moderno
+      { url: '/TheShowVerse2.png', type: 'image/png', sizes: '32x32' },
+      { url: '/TheShowVerse2.png', type: 'image/png', sizes: '16x16' },
     ],
-    shortcut: ['/favicon.ico'],
+    shortcut: ['/TheShowVerse2.png'],
     apple: [{ url: '/TheShowVerse2.png', sizes: '180x180' }], // iOS (app)
   },
   manifest: '/site.webmanifest',
